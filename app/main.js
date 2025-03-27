64a601fdab7787030a934e8ffa47824589aa568a
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-03-27 09:05:42 -0300

    Aplicando desconto

--- app/main.js
+++ app/main.js
@@ -1,29 +1,10 @@
 let livros = [];
 const endpointDaAPI = 'https://raw.githubusercontent.com/MarcusViniciusGodoy/livrosJson/refs/heads/main/livros.json';
 getBuscaLivrosDaAPI();
-const elementoParaInserirLivros = document.getElementById('livros');
 
 async function getBuscaLivrosDaAPI() {
     const resposta = await fetch(endpointDaAPI);
     livros = await resposta.json();
-    console.table(livros);
-    exibirLivrosTela(livros);
-};
-
-function exibirLivrosTela(listaDeLivros){
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-            ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco}</p>
-        <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-        </div>
-        </div>
-        `
-    });
+    let livrosDesconto = aplicarDesconto(livros);
+    exibirLivrosTela(livrosDesconto);
 };
