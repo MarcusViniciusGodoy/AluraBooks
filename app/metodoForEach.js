9cb6d8161ba559eb96611b469d0eb22933ee4a1a
MarcusViniciusGodoy <marcusviniciusdegodoy@hotmail.com>
2025-03-27 10:47:53 -0300

    Filtro de disponibilidade de livros

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,9 +3,10 @@ const elementoParaInserirLivros = document.getElementById('livros');
 function exibirLivrosTela(listaDeLivros){
     elementoParaInserirLivros.innerHTML = '';
     listaDeLivros.forEach(livro => {
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagem' : 'livro_imagens indisponivel';
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
         <h2 class="livro__titulo">
             ${livro.titulo}
         </h2>
